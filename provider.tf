9acbbff2e71e4ea7ca0ad5c51f3d62ecc52db804
ignis <maildevcom+1@gmail.com>
2024-07-17 22:28:46 -0400

    feat: - providers have been moved out of main.tf;

--- provider.tf
+++ provider.tf
@@ -1,3 +1,26 @@
-# provider "aws" {
-#   region = var.region
-# }
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.58.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.31.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.14.0"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
